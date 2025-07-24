f3e3a48b6337dfb32fbf33f354a86861774fdd2d
ferdyleoza <ferdyleoza123@gmail.com>
2025-07-24 15:32:07 +0700

    refactor: update allowed origins in CORS configuration

--- config/cors.go
+++ config/cors.go
@@ -3,7 +3,7 @@ package config
 var AllowedOrigins = []string{
 	"http://localhost:6969",
 	"http://localhost:5173",
-	"https://blogger-go.netlify.app",
+	"https://tampilan-blog.vercel.app",
 	"https://backendblog.up.railway.app",
 }
 
