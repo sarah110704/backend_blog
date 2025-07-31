bf974829f2a6f8d7ba8e637c051377cca2e00c03
sarah110704 <sarahyohana439@gmail.com>
2025-07-31 21:23:55 +0700

    commit

--- config/cors.go
+++ config/cors.go
@@ -1,7 +1,7 @@
 package config
 
 var AllowedOrigins = []string{
-	"https://localhost:6969",
+	"http://localhost:6969",
 	"http://localhost:5173",
 	"https://tampilan-blog.vercel.app",
 	"https://backendblog.up.railway.app",
