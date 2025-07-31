fdf5f32df1b0201d0ba50fbd6df5e94afebdd5da
sarah110704 <sarahyohana439@gmail.com>
2025-07-31 21:23:37 +0700

    commit

--- docs/docs.go
+++ docs/docs.go
@@ -1169,7 +1169,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:6969",
+	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{"http", "https"},
 	Title:            "Backend Artikel API",
